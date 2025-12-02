11eab766b0e7061ec8a2d8a0c3df56cd073b66c2
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2025-12-02 13:50:04 +0100

    Initial commit
    Copy of GameEngine-A2

---
+++ GEMemory/GEMemory/main.cpp
@@ -0,0 +1,18 @@
+#include "TestCases.h"
+#include "Interface.h"
+
+#include "raylib.h"
+
+int main() {
+	Interface interface;
+
+	while (!WindowShouldClose()) {
+		interface.Update();
+	}
+
+	CloseWindow();
+
+	//PoolVSOS();
+	TestAll();
+	return 0;
+}
