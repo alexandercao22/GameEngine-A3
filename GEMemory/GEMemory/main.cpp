c495624799e68c5d0a47511fc8a24f3ec2d9803c
Arvid Wahlgren <salliban03@gmail.com>
2025-12-04 14:52:59 +0100

    Some filesystem testing

--- GEMemory/GEMemory/main.cpp
+++ GEMemory/GEMemory/main.cpp
@@ -3,16 +3,30 @@
 
 #include "raylib.h"
 
+#include <filesystem>
+
 int main() {
-	Interface interface;
+	//Interface interface;
 
-	while (!WindowShouldClose()) {
-		interface.Update();
-	}
+	//while (!WindowShouldClose()) {
+	//	interface.Update();
+	//}
 
-	CloseWindow();
+	//CloseWindow();
 
 	//PoolVSOS();
-	TestAll();
+	//TestAll();
+
+
+	// Filesystem testing
+
+	std::string relative = "Hello.csv";
+	std::filesystem::path relativePath(relative);
+	std::filesystem::path p("/project/assets");
+
+	std::filesystem::path fullPath = p / relativePath;
+	std::cout << fullPath.string() << std::endl;
+	
+
 	return 0;
 }
