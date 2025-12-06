de9dd1519deba76b6cac449834c1c6f98e56e977
Arvid Wahlgren <salliban03@gmail.com>
2025-12-06 18:15:50 +0100

    GuidUtils functionality

--- GEMemory/GEMemory/main.cpp
+++ GEMemory/GEMemory/main.cpp
@@ -1,6 +1,7 @@
 #include "TestCases.h"
 #include "Interface.h"
 #include "PackageManager.h"
+#include "GuidUtils.h"
 
 #include "raylib.h"
 
@@ -20,13 +21,19 @@ int main() {
 	//TestAll();
 
 	// PackageManager tests
-	PackageManager pm;
+	//PackageManager pm;
 	//pm.Pack("../GEMemory/Resources/level1", "../GEMemory/Resources");
-	pm.Unpack("../GEMemory/Resources/level1.gepak", "../GEMemory/Resources");
-	pm.MountPackage("../GEMemory/Resources/level1.gepak");
-	AssetData asset;
-	pm.LoadAsset("293088.png", "level1", asset);
-	pm.UnmountPackage("level1");
+	//pm.Unpack("../GEMemory/Resources/level1.gepak", "../GEMemory/Resources");
+	//pm.MountPackage("../GEMemory/Resources/level1.gepak");
+	//AssetData asset;
+	//pm.LoadAsset("293088.png", "level1", asset);
+	//pm.UnmountPackage("level1");
+
+	std::string guid;
+	GuidUtils::GetOrGenerateGuid(std::filesystem::path("../GEMemory/Resources/level1/293088.png"), guid);
+	std::cout << "Generated GUID: " << guid << std::endl;
+
+
 
 	return 0;
 }
