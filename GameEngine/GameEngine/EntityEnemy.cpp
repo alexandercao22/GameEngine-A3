ff24c5a2b29b3a0b277606c969c3c03a79e30b4b
alexandercao22 <144004304+alexandercao22@users.noreply.github.com>
2025-12-08 15:57:02 +0100

    Use revamped RM functions

--- GameEngine/GameEngine/EntityEnemy.cpp
+++ GameEngine/GameEngine/EntityEnemy.cpp
@@ -1,17 +1,28 @@
 #include "EntityEnemy.h"
 
 #include "ResourceManager.h"
+#include "Resource.h"
 
 EntityEnemy::~EntityEnemy()
 {
-    ResourceManager::Instance().Unload("27471003-7f27-42e3-a791-c159b33f5198");
-    ResourceManager::Instance().Unload("");
+    ResourceManager::Instance().UnloadResource("27471003-7f27-42e3-a791-c159b33f5198");
+    ResourceManager::Instance().UnloadResource("");
 }
 
 bool EntityEnemy::Init()
 {
-    _mesh = (MeshResource *)ResourceManager::Instance().Load("27471003-7f27-42e3-a791-c159b33f5198");
-    _texture = (TextureResource *)ResourceManager::Instance().Load("");
+    if (!ResourceManager::Instance().LoadResource("27471003-7f27-42e3-a791-c159b33f5198", _mesh)) {
+        std::cerr << "EntityEnemy::Init(): Failed to load mesh" << std::endl;
+        return false;
+    }
+    if (!ResourceManager::Instance().LoadResource("", _texture)) {
+        std::cerr << "EntityEnemy::Init(): Failed to load texture" << std::endl;
+        return false;
+    }
+
+    _mesh->Init();
+    _texture->Init();
+
     return true;
 }
 
