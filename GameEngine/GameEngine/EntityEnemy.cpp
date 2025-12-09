e021f6885201c9f9923c394dbf1ecee3aee6100a
Arvid Wahlgren <salliban03@gmail.com>
2025-12-09 15:55:42 +0100

    Removed deprecated code

--- GameEngine/GameEngine/EntityEnemy.cpp
+++ GameEngine/GameEngine/EntityEnemy.cpp
@@ -20,7 +20,6 @@ bool EntityEnemy::Init()
         return false;
     }
     _mesh = (MeshResource *)mesh;
-    _mesh->Init();
 
     Resource *texture = new TextureResource;
     if (!ResourceManager::Instance().LoadResource("", texture)) {
@@ -29,7 +28,6 @@ bool EntityEnemy::Init()
     }
     else {
         _texture = (TextureResource *)texture;
-        _texture->Init();
     }
 
     return true;
